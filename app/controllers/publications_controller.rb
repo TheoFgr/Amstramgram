f6da40220f2266acc0ddf55d3553d2c56328a412
Theo Faugere <theo.faugere3@gmail.com>
2022-04-17 13:54:23 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -2,4 +2,21 @@ class PublicationsController < ApplicationController
   def index
     @publications = Publication.all
   end
+
+
+
+  def new
+    @publication = Publication.new
+  end
+
+  def create
+    @publication = Publication.new(publication_params)
+    @publication.save
+  end
+
+  private
+
+  def publication_params
+    params.require(:publication).permit(:picture, :content, :number_of_like)
+  end
 end
